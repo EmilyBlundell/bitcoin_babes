64dc5a6cc70059785c6445e8e9f84254750134e2
EmilyBlundell <emandblund@gmail.com>
2022-08-09 15:17:46 +0100

    Simulator and main update

--- client/main.py
+++ client/main.py
@@ -1,8 +1,9 @@
 import requests
 import json
+from simulator import *
+from collections import namedtuple
 
-
-def get_best_trader():
+'''def get_best_trader():
 
 
 def get_worst_trader():
@@ -14,7 +15,7 @@ def display_all_traders():
 def get_trader_stat(name):
 
 
-def get_trader_group(trader_score):
+def get_trader_group(trader_score):'''
 
 def add_trader(first, last, score, crypto):
     trader = {
@@ -38,7 +39,42 @@ def run():
     print('############################')
     print()
 
-    ### Logic for trading simulator goes here ###
+    initial_crypto = 100
+
+    print('Welcome to the trading simulator\nYou have 100 units of Bitcoin (BTC) to start trading with')
+
+    first = input('Please enter your first name')
+    last = input('Please enter your last name')
+
+    #a = investment('BTC', 50)
+
+    #first_play = switch_currency(initial_crypto)
+
+    #print(type(first_play))
+
+    #currency, amount = first_play
+
+    stock = investment(switch_currency(initial_crypto))
+
+    #a.gamble()
+
+    #a.fluctuate()
+
+    stock.gamble()
+
+    a.fluctuate()
+
+    a.free_crypto()
+
+    print(f'After market fluctuations your final amount of {a.currency} is {a.amount}')
+
+    print(f'You final score on this simulator is {a.score} meaning you have earnt {a.crypto} units of {a.currency}')
+
+    new_trader = namedtuple('trader', ['first', 'last', 'score', 'crypto', 'currency'])
+
+    newest_trader = new_trader(first, last, a.score, a.crypto, a.currency)
+
+
 
 if __name__ == '__main__':
     run()
--- client/simulator.py
+++ client/simulator.py
@@ -1,27 +1,57 @@
-'''
-
-This code will be used in the main.py file for the client to interact with
-
-
-'''
 
 import requests
 import time
 import random
+import itertools
+import math
 
 class investment():
 
     def __init__(self, currency, amount):
           self.currency = currency
           self.amount = amount
+          self.score = 0
+          self.crypto = 0
 
     def fluctuate(self):
 
         fluctuation = round(random.uniform(0, 2), 2)
-        self.amount = self.amount * fluctuation
+        self.amount = round((int(self.amount) * fluctuation), 2)
         print(f'There has been a {fluctuation} fluctuation in the market\nYour new balance of {self.currency} is {self.amount}')
 
 
+    def gamble(self):
+
+        take_chance = input('''Would you like to enter a random gamble?
+        \nThis function will scramble the numbers of your current balance, potentially to your advantage: y/n''')
+
+        if take_chance == 'y':
+
+            iter_amount = str(self.amount)
+
+            gambles = list(itertools.permutations(iter_amount))
+
+            possibilities = num_permutations(len(iter_amount))
+
+            choice = random.randint(0, possibilities-1)
+
+            self.amount = gambles[choice]
+
+            self.amount = "".join(self.amount)
+
+            print(f'Your new total of {self.currency} is {self.amount}')
+
+    def free_crypto(self):
+
+        self.score = round(math.log(self.amount, 2), 2)
+        self.crypto = round((self.score **2), 2)
+
+
+def num_permutations(n):
+        if n == 1:
+            return n
+        else:
+            return n * num_permutations(n - 1)
 
 def timer(func):
 
@@ -36,40 +66,20 @@ def timer(func):
             time.sleep(1)
             t += 1
 
-        print('API reqeuests ready again')
+        print('API requests ready again')
 
     return inner_wrapper
-
-'''def market_fluctuation(func):
-
-    def inner_wrapper(*args, **kwargs):
-
-        func(*args, **kwargs)
-
-        fluctuation = random.randint(0,1)
-
-        if fluctuation:
-
-            crypto= crypto *0.75
-
-        else:
-
-
-
-    return inner_wrapper
-'''
-
-
-crypto = 100
-
-available_currencies = ['ETH', 'USDT','BNB', 'XRP']
-
-print('Welcome to the trading simulator\nYou have 100 units of Bitcoin (BTC) to start trading with')
 
 @timer
 def switch_currency(crypto):
 
     switch_currency = input('Would you like to start trading in another digital currency instead?\nEnter y or n')
+
+    crypto = crypto
+
+    new_currency = 'BTC'
+
+    available_currencies = ['ETH', 'USDT', 'BNB', 'XRP']
 
     if switch_currency == 'y':
 
@@ -93,24 +103,7 @@ def switch_currency(crypto):
         crypto = crypto * exchange_rate
         print(f'You now have {crypto} units of {new_currency}')
 
-
-def invest():
-
-
-
-
-
-
-
-
-
-'''
-from collections import namedtuple
-
-score = simulate()
-
-new_trader = namedtuple('trader', ['first', 'last', 'score', 'crypto'])'''
-
+    return new_currency, crypto
 
 
 
