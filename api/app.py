69a88b7711b945d350b5cbc9b6a4b2c9d27cb14b
ImanKasmani <92366292+ImanKasmani@users.noreply.github.com>
2022-08-10 15:56:31 +0100

    update db_utils & app

--- api/app.py
+++ api/app.py
@@ -4,23 +4,44 @@ import api.db_utils as dbutils
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return '<h1>Welcome to Test Trade, the add-on for validating trading prowess</h1>'
 
-@app.route('/best')
-def get_availability(date):
-    trader = dbutils.get_best_trader()
-    return jsonify(trader)
+
+@app.route('/leader/<selection>')
+def get_best_or_worst(selection):
+    if selection == 'best':
+        trader = dbutils.get_best_trader()
+        return jsonify(trader)
+    elif selection == 'worst':
+        trader = dbutils.get_worst_trader()
+        return jsonify(trader)
+    else:
+        return 'Wrong input'
+
 
 @app.route('/new', methods=['PUT'])
 def new_trader():
     trader = request.get_json()
     dbutils.add_trader(
-        first = trader['First_Name'],
-        last = trader['Last_Name'],
-        score = trader['Score'],
-        crypto = trader['Current_Balance']
+        first=trader['First_Name'],
+        last=trader['Last_Name'],
+        score=trader['Score'],
+        crypto=trader['Current_Balance']
+    )
+    return trader
+
+
+@app.route('/user/<int:investor_id>')
+def get_user_info():
+    trader = request.get_json()
+    dbutils.get_trader_stat(
+        ranking=trader['ranking'],
+        investor_id=trader['investor_id'],
+        first_name=trader['First_name'],
+        score=trader['score']
     )
     return trader
 
