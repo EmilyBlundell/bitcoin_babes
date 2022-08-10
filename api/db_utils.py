6af712508ebbca313866a327b918defe1e269d79
Frank Promise Edah <99150197+Frankpromise@users.noreply.github.com>
2022-08-10 10:24:36 +0200

    Update db_utils.py

--- api/db_utils.py
+++ api/db_utils.py
@@ -15,7 +15,6 @@ def _connect_to_db(db_name):
     )
     return connect
 
-
 def _map_values(traders):
     mapped = []
     for profile in traders:
@@ -26,20 +25,21 @@ def _map_values(traders):
         })
     return mapped
 
-# EXAMPLE 1
-
-
+# EXAMPLE 1 - Gets the trader/ traders with the highest score
 def get_best_trader():
     try:
         nanodb = _connect_to_db('nano')
         cur = nanodb.cursor()
-        query = """ INSERT SQL QUERY HERE"""
+        query = """ SELECT investor_id, current_score
+FROM investors_info
+WHERE current_score IN (SELECT MAX(current_score)
+						FROM investors_info);"""
         cur.execute(query)
 
         best_trader = _map_values(cur.fetchall())
 
     except Exception:
-        raise DbConnectionError("Failed to connect to the database")
+        raise  DbConnectionError("Failed to connect to the database")
     finally:
         if nanodb:
             nanodb.close()
@@ -47,12 +47,15 @@ def get_best_trader():
 
     return best_trader
 
-
+# Gets the trader/ traders with the lowest score
 def get_worst_trader():
     try:
         nanodb = _connect_to_db('nano')
         cur = nanodb.cursor()
-        query = """ INSERT SQL QUERY HERE"""
+        query = """ SELECT investor_id, current_score
+FROM investors_info
+WHERE current_score IN (SELECT MIN(current_score)
+						FROM investors_info);"""
         cur.execute(query)
 
         worst_trader = _map_values(cur.fetchall())
@@ -71,7 +74,8 @@ def display_all_traders():
     try:
         nanodb = _connect_to_db('nano')
         cur = nanodb.cursor()
-        query = """ INSERT SQL QUERY HERE"""
+        query = """ SELECT investor_id, current_score, crypto_balance
+            FROM investors_info;"""
         cur.execute(query)
 
         all_traders = _map_values(cur.fetchall())
@@ -85,12 +89,14 @@ def display_all_traders():
 
     return all_traders
 
-
-def get_trader_stat():
+### need to make an id variable ###
+def get_trader_stat(id):
     try:
         nanodb = _connect_to_db('nano')
         cur = nanodb.cursor()
-        query = """ INSERT SQL QUERY HERE"""
+        query = """ SELECT investor_id, current_score, crypto_balance
+        FROM investors_info
+        WHERE investor_id = {}; """.format(id)
         cur.execute(query)
 
         trader_stat = _map_values(cur.fetchall())
@@ -109,7 +115,9 @@ def get_trader_group(trader_score):
     try:
         nanodb = _connect_to_db('nano')
         cur = nanodb.cursor()
-        query = """ INSERT SQL QUERY HERE"""
+        query = """ SELECT investor_id, current_score
+        FROM investors_info
+        WHERE current_score = {};""".format(trader_score)
         cur.execute(query)
 
         all_traders = _map_values(cur.fetchall())
@@ -123,13 +131,13 @@ def get_trader_group(trader_score):
 
     return all_traders
 
-
-def add_trader(first, last, score, crypto):
+### earlier used trader_score as variable
+def add_trader(id, first, last, score, crypto):
     try:
         nanodb = _connect_to_db('nano')
         cur = nanodb.cursor()
-        update_booking = """INSERT SQL QUERY""".format(
-            first=first, last=last, score=score, crypto=crypto)
+        update_booking = """INSERT INTO investors_info 
+        VALUES({}, {}, {}, {},{})""".format(id, first, last, score, crypto)
         cur.execute(update_booking)
 
         nanodb.commit()
@@ -140,51 +148,5 @@ def add_trader(first, last, score, crypto):
         if nanodb:
             nanodb.close()
 
-
-def get_leader_board():
-    try:
-        nanodb = _connect_to_db('nano')
-        cur = nanodb.cursor()
-        query = """ 
-        # CREATE INDEX - will have large amounts of data 
-        # SELECT ....
-        # FROM ....
-        # LEFT JOIN ....
-        # ON x = y
-        # GROUP BY ...
-        # ORDER BY points DESC LIMIT 3;
-        # """.format
-        # cur.execute(query)
-
-        top_3_users = _map_values(cur.fetchall())
-    except Exception:
-        raise DbConnectionError("Failed to read from the database")
-    finally:
-        if nanodb:
-            nanodb.close()
-            print('DB connection closed')
-
-    return top_3_users
-
-
-def add_points(username, additional_points):
-    try:
-        nanodb = _connect_to_db('nano')
-        cur = nanodb.cursor()
-        update_users = """
-        # need to add points to current point in table but do not want to make new column
-        UPDATE user SET current_points = current_points + {additional_pints} WHERE user_id/name = {username}  ???? - need to check this 
-        """.format(username=username, additional_points=additional_points)
-        cur.execute(update_users)
-
-        nanodb.commit()
-        cur.close()
-    except Exception:
-        raise DbConnectionError("DB connection failed")
-    finally:
-        if nanodb:
-            nanodb.close()
-
-
 if __name__ == '__main__':
-    print(display_all_traders())
+   print( display_all_traders())
