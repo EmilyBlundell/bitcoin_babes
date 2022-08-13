bdba01c4dd74accc88c456e5652c9046e37c2efa
Rebecca <rebeccajclarke1@gmail.com>
2022-08-13 20:45:05 +0100

    Updated the database with more realistic data

--- db/create_nano_db_script.sql
+++ db/create_nano_db_script.sql
@@ -1,108 +1,52 @@
-CREATE DATABASE nano;
-
+CREATE DATABASE IF NOT EXISTS nano;
 USE nano;
-
-DROP TABLE investors_info;
-
 -- Creation of investor info table
-
-CREATE TABLE
+CREATE TABLE IF NOT EXISTS
     investors_info (
         investor_id INTEGER NOT NULL,
         investor_first_name VARCHAR(45) NOT NULL,
         investor_last_name VARCHAR(45) NOT NULL,
-        current_score INTEGER NOT NULL,
-        crypto_balance DECIMAL NOT NULL,
+        current_score FLOAT NOT NULL,
+        crypto_balance FLOAT NOT NULL,
         currency VARCHAR(15) NOT NULL,
         CONSTRAINT PK_trader_info PRIMARY KEY (investor_ID)
     );
-
 -- inserting dummy data
-
 INSERT INTO investors_info
-VALUES (1, 'John', 'Smith', 6, 600, 'BTC'), (
-        2,
-        'Kate',
-        'Booker',
-        9,
-        900,
-        'ETH'
-    ), (
-        3,
-        'Shannon',
-        'Burton',
-        7,
-        700,
-        'BNB'
-    ), (
-        4,
-        'Joshua',
-        'Medley',
-        4,
-        400,
-        'BNB'
-    ), (
-        5,
-        'Tracey',
-        'James',
-        3,
-        300,
-        'XRP'
-    ), (
-        6,
-        'Chris',
-        'Bailey',
-        2,
-        200,
-        'ETH'
-    );
+VALUES
+(1, 'John', 'Smith', 5.94, 35.28, 'BTC'),
+(2, 'Kate', 'Booker', 9.9, 98.01, 'ETH'),
+(3, 'Shannon', 'Burton', 13.54, 183.33, 'BNB'),
+(4, 'Joshua', 'Medley', 22.3, 497.29, 'XRP'),
+(5, 'Tracey', 'James', 9.23, 85.19, 'ETH'),
+(6, 'Chris', 'Bailey', 12.93, 167.18, 'ETH');
 
-USE nano;
-
-ALTER TABLE investors_info ADD currency VARCHAR(15);
-
-UPDATE investors_info SET currency = 'BTC' WHERE investor_id = 1;
-
-UPDATE investors_info SET currency = 'ETH' WHERE investor_id = 2;
-
-UPDATE investors_info SET currency = 'XRP' WHERE investor_id = 3;
-
-UPDATE investors_info SET currency = 'BNB' WHERE investor_id = 4;
-
-UPDATE investors_info SET currency = 'BTC' WHERE investor_id = 5;
 
 -- stored procedure to fill data on investors
-
 USE nano;
-
 DELIMITER $$
-
 CREATE DEFINER =`root`@`localhost` PROCEDURE `fillinvestor`
-(IN id INTEGER, IN first_name VARCHAR(45), last_name 
-VARCHAR(45), score integer, IN crypto decimal) 
-BEGIN 
+(IN id INTEGER, IN first_name VARCHAR(45), last_name VARCHAR(45), score integer, IN crypto decimal, currency VARCHAR(15))
+BEGIN
 	INSERT INTO
 	    investors_info (
 	        investor_id,
 	        investor_first_name,
 	        investor_last_name,
 	        current_score,
-	        crypto_balance
+	        crypto_balance,
+            currency
 	    )
 	VALUES (
 	        id,
 	        first_name,
 	        last_name,
 	        score,
-	        crypto
+	        crypto,
+            currency
 	    );
-	E 
-END$$ 
 
+END$$
 DELIMITER ;
-
 -- adding a new investor using stored procedure
-
 CALL fillinvestor(7, 'Laura', 'Green', 9, 80.02, 'ETH');
-
-Footer 
