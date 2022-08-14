fcd300ba3f7bf33920b3e24aa5924813c1073cfb
ImanKasmani <92366292+ImanKasmani@users.noreply.github.com>
2022-08-14 20:43:23 +0100

    update

--- client/main.py
+++ client/main.py
@@ -61,11 +61,11 @@ def get_new_trader_id():
 
 
 # function to add a new trader
-def add_trader(new_id, first, last, score, crypto, currency):
+def add_trader(investor_id, first_name, last_name, score, crypto, currency):
     trader = {
-        "investor_id": new_id,
-        "investor_first_name": first,
-        "investor_last_name": last,
+        "investor_id": investor_id,
+        "investor_first_name": first_name,
+        "investor_last_name": last_name,
         "crypto_score": score,
         "current_balance": crypto,
         "currency": currency
@@ -94,8 +94,8 @@ def run():
     print('############################')
     print()
 
-    first = input('Please enter your first name ')
-    last = input('Please enter your last name ')
+    first_name = input('Please enter your first name ')
+    last_name = input('Please enter your last name ')
     print('############################')
     print()
 
@@ -134,17 +134,18 @@ def run():
     new_trader = namedtuple(
         'trader', ['id', 'first', 'last', 'score', 'crypto', 'currency'])
 
-    new_id = get_new_trader_id() + 1
+    investor_id = get_new_trader_id() + 1
 
     newest_trader = new_trader(
-        new_id, first, last, stock.score, stock.crypto, stock.currency)
+        investor_id, first_name, last_name, stock.score, stock.crypto, stock.currency)
 
     print(
         f'Welcome new trader\nYour registration details are as follows: {newest_trader}')
     print('############################')
     print()
 
-    add_trader(new_id, first, last, stock.score, stock.crypto, stock.currency)
+    add_trader(investor_id, first_name, last_name,
+               stock.score, stock.crypto, stock.currency)
 
 
 # function to view if users would like to obtain the best or worst trader, all traders or a specific trader
